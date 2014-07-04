4a51d7b2d9d509bc2327e0d9dd8e8f1c67401d92
Eugene Leshchynskyy <eleschynskyy@gmail.com>
2014-07-04 17:15:46 +0300

    proxy branch created

--- src/main/java/com/br/core/DriverMaster.java
+++ src/main/java/com/br/core/DriverMaster.java
@@ -26,6 +26,7 @@ public class DriverMaster {
 	};
 
 	public static WebDriver startDriverInstance(String driverKey) {
+		//proxy
 		BrowserType browser = BrowserType.get(driverKey);
 		WebDriver driver;
 		EventFiringWebDriver eventDriver;
