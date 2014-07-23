31cf6e85037a89b2075eda59b8537134c869bc92
Eugene Leshchynskyy <eleschynskyy@gmail.com>
2014-07-23 16:37:26 +0300

    adding TestGoogle.java

--- src/test/java/com/br/tests/TestGoogle.java
+++ src/test/java/com/br/tests/TestGoogle.java
@@ -4,12 +4,23 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.Test;
 
+import com.br.core.BaseTest;
+import com.br.core.DriverMaster;
+import com.br.core.web.pages.HomePage;
+import com.br.core.web.pages.LoginPage;
+
 public class TestGoogle {
 	
 	@Test
 	public void userWithRightCredentialsCouldLogin() {
 		WebDriver driver = new InternetExplorerDriver(); 
 		driver.get("http://google.com");
+//		LoginPage loginPage = new LoginPage(DriverMaster.getDriverInstance());
+//		try {
+//			Thread.sleep(3000);
+//		} catch (InterruptedException e) {
+//			e.printStackTrace();
+//		}
 	}
 	
 }
